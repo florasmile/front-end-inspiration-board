f87cd395eb8990ebf809bbb0debce76d8619a5b6
dehuihu <dehuihu@gmail.com>
2025-06-25 04:31:12 -0700

    Added CardList and Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,19 @@
-const Card = () => {
-  return ;
+import PropTypes from 'prop-types';
+const Card = ({ id, message }) => {
+
+  return <li className="card-container">
+    <p>{message}</p>
+    <div>
+      <span>💕</span>
+      <button>+1</button>
+      <button>delete</button>
+    </div>    
+  </li>;
 };
 
+
+Card.propTypes = {
+  id: PropTypes.number.isRequired,
+  message: PropTypes.string.isRequired,
+};
 export default Card;
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,5 +1,30 @@
-const CardList = () => {
-  return ;
+import PropTypes from 'prop-types';
+import Card from './Card';
+
+const CardList = ({ cards }) => {
+  const getCardListJSX = (cards) => {
+    return cards.map((card) => {
+      return (
+        <Card
+          key={card.id}
+          id={card.id}
+          message={card.message}
+        />
+      );
+    })
+  };
+
+  return <ul className="cardlist-container">
+    { getCardListJSX (cards)}
+  </ul>
 };
 
+CardList.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      message: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 export default CardList;
