88d28b4716e6481f06a507369b05e3f8814cfe26
dehuihu <dehuihu@gmail.com>
2025-06-25 04:11:31 -0700

    Implemented BoardList, Board component

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,37 @@ import CardList from './components/CardList';
 import NewBoardForm from './components/NewBoardForm';
 import NewCardForm from './components/NewCardForm';
 import BOARDS from './boards.json';
+import CARDS from './cards.json';
 
 function App() {
-  const [count, setCount] = useState(0)
   const [boards, setBoards] = useState(BOARDS)
+  const [curBoard, setCurBoard] = useState(boards[0])
+  const [cards, setCards] = useState(CARDS)
 
+  const displayBoard = (id) => {
+    // when a board is selected, we want to display its title, owner's name, and all cards;
+    setCurBoard(() => {
+      const result = boards.filter(board => board.id === id)
+      console.log(result[0]);
+      return result[0];
+    });
+    // send API calls to get a list of cards of current board to display; and call setCards
+
+  };
   return (
     <>
       <header>
         <h1>Inspiration Board</h1>
       </header>
-      <BoardList boards={boards}/>
-      <CardList />
+      <BoardList boards={boards} displayBoard={displayBoard}/>
+      <section>
+        <h2>Current Board</h2>
+        <p>{curBoard.title} - {curBoard.owner}</p>
+        <CardList cards={cards}/>
+      </section>      
       <NewBoardForm />
       <NewCardForm />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
       </div>
     </>
   )
--- src/components/BoardList.jsx
+++ src/components/BoardList.jsx
@@ -1,5 +1,34 @@
-const BoardList = () => {
-  return ;
+import PropTypes from 'prop-types';
+import Board from './Board.jsx';
+
+const BoardList = ({ boards, displayBoard }) => {
+  const getBoardListJSX = (boards) => {
+    return boards.map((board) => {
+      return (
+        <Board 
+          key={board.id}
+          id={board.id}
+          title={board.title}
+          // owner={board.owner}
+          displayBoard={displayBoard}
+        />
+      );
+    })
+  };
+  return <ul>
+    { getBoardListJSX(boards)}
+  </ul>
+};
+
+BoardList.propTypes = {
+  boards: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      // owner: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  displayBoard: PropTypes.func.isRequired,
 };
 
 export default BoardList;
