9f0d806641bf49337b897fa961c173385f13ad16
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-06-30 17:51:11 -0700

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
-  base: '/front-end-inspiration-board/', // Add this line (match your repo name)
+  base: '/front-end-inspiration-board/',
   server: {
     allowedHosts: ['front-end-inspiration-board-r4rm.onrender.com'],
   },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+  }
 })
