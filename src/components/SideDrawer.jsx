82f07537f6d9cdce352b945a60596880b1ac71e4
dehuihu <dehuihu@gmail.com>
2025-07-07 07:13:25 -0700

    Adds + create new form and hide form in SideDrawer

--- src/components/SideDrawer.jsx
+++ src/components/SideDrawer.jsx
@@ -1,21 +1,48 @@
 import PropTypes from 'prop-types';
 import './SideDrawer.css';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 import NewBoardForm from './NewBoardForm';
 import NewCardForm from './NewCardForm';
 import MoodSelector from './MoodSelector';
 
 const SideDrawer = ({ onPostBoard, onPostCard, onChangeMood, isOpen, closeDrawer }) => {
-
+    const [showBoardForm, setShowBoardForm] = useState(false);
+    const [showCardForm, setShowCardForm] = useState(false);
+    const toggleBoardFormDisplay = () => {
+        setShowBoardForm(showBoardForm => !showBoardForm);
+    };
+    const toggleCardFormDisplay = () => {
+        setShowCardForm(showCardForm => !showCardForm);
+    };
     return (
         <aside className={`side-drawer ${isOpen ? 'open' : ''}`}>
             <button className="close-button" onClick={closeDrawer}>❌</button>
             {/* Board section */}
-            <NewBoardForm onPostBoard={onPostBoard} isOpen={isOpen} />
-            <NewCardForm onPostCard={onPostCard}/>
-            {/* Card section */}
-             {/* MOOD SECTION */}
+        <div>
+            {
+            !showBoardForm && 
+            <button onClick={toggleBoardFormDisplay}>+ Create a new board</button>  
+            } 
+            {showBoardForm && 
+            <NewBoardForm onPostBoard={onPostBoard}/>
+            }
+            {showBoardForm && 
+            <button onClick={toggleBoardFormDisplay}>hide board form</button>
+            }
+        </div>
+        <div>
+            {
+            !showCardForm && 
+                <button onClick={toggleCardFormDisplay}>+ Create a new Card</button>  
+            } 
+            {showCardForm && 
+                <NewCardForm onPostCard={onPostCard}/>
+            }
+            {showCardForm && 
+            <button onClick={toggleCardFormDisplay}>hide card form</button>}
+        </div>
+            {/* MOOD SECTION */}
             <MoodSelector onChangeMood={onChangeMood} />
         </aside>
     );
